3ba8f3f385ebab8c6d722c07911aac1a59d2b3ea
Paulo Henrique Castro <phlcastro@gmail.com>
2016-05-11 03:02:56 +0000

    calculating roll points according to the rules

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,5 @@
 class Dice
   def self.roll(qtt)
-    qtt.times.map{Random.rand(1..6)} if (qtt.is_a? Integer) && qtt > 0
+    Array.new(qtt) { Random.rand(1..6) } if qtt.is_a?(Integer) && qtt > 0
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -6,25 +6,24 @@ describe Dice do
       it 'returns an array object' do
         expect(Dice.roll(5)).to be_a Array
       end
-      
+
       it 'returns an array of specified size' do
         expect(Dice.roll(5).length).to eq(5)
       end
     end
-    
+
     context 'when receives an invalid parameter' do
       it 'returns nil for strings' do
         expect(Dice.roll('5')).to be_nil
       end
-      
+
       it 'returns nil for values equal zero' do
         expect(Dice.roll(0)).to be_nil
       end
-      
+
       it 'returns nil for values less than zero' do
         expect(Dice.roll(-1)).to be_nil
       end
     end
   end
-  
 end
